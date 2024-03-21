7770ba4586f97a67ea93bb924181e1e4fcf80f1a
DefaultGitter <burlonepazzo@gmail.com>
2024-03-21 14:38:51 +0200

    2 hour

--- Base/collection.py
+++ Base/collection.py
@@ -3,11 +3,36 @@
 list_1 = [i for i in range(10)]  # генерация списка
 list_2 = (i for i in range(10))  # генерация списка | list comprehension
 
-print(f'list_1: {list_1}\n'
-      f'{type(list_1)}\n')
+out = 1
+if out:
+    print(f'lis_1: {list_1}\n'
+          f'{type(list_1)}\n')
 
-print(f'list_2: {list_2}\n'
-      f'{type(list_2)}')
+    print(f'list_2: {list_2}\n'
+          f'{type(list_2)}')
+
+# Если присвоить список переменной,
+# то в случае измения копии будет менятся и оригинал
+# Для создания независимой копии
+# С сохранением всех параметров и типов используется метод .copy()
+l_cp = list_1.copy()
+# Для клонирования с сохранением только содержимого можно использовать конструктор классов
+l_copy = list(list_1)
+
+l_copy.append('anything')
+print(l_copy)
+
+l_copy.reverse()
+print(l_copy)
+
+# l_copy['index']
+print(f'Fourth element: {l_copy[4]}')
+# l_copy['start', 'end', 'step']
+print(f'List from second element to last, reversed: {l_copy[2::-1]}')
+print(f'Every second: {', '.join(map(str, l_copy[::2])):>25}')
+# Метод 'separator'.join(list) работает только на элементах типа str
+# Для обьединения числовой коллекции нужно переформатировать элементы в строковый тип
+# через функцию map(конструктор класса, список)
 
 # var1 = ("word1", "word2", [1, 3])  # tuple
 # var1[2][0] = 42
